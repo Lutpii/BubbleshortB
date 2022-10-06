e92be005ec47e005608b6c702d04c2bf2b62010e
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-10-06 17:45:29 +0700

    Method BubbleSortArray

--- BubbleshortB/Program.cs
+++ BubbleshortB/Program.cs
@@ -52,5 +52,23 @@ namespace BubbleshortB
             }
             Console.WriteLine("");
         }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) // For n - 1 passes
+            {
+                //Pada pass i, bandingkan n-i elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j <= n; j++)
+                {
+                    if (a[j] < a[j - 1]) // Jika elemen tidak dalam urutan yang benar
+                    {
+                        //Tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
